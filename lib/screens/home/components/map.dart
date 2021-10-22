df15c1370e2dc765ad0b7d08e99fc21a6166637d
youngmi97 <jaehwan@sureplus.io>
2021-10-22 13:27:31 +0900

    add naver maps dependency

--- lib/screens/home/components/map.dart
+++ lib/screens/home/components/map.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:naver_map_plugin/naver_map_plugin.dart';
 
 class Map extends StatelessWidget {
   const Map({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Text(
-      'Index 2: School',
-    );
+    return NaverMap();
   }
 }
